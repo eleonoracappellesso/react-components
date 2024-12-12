674ecd85943f722465789dbfb217c76efb80f92d
eleonoracappellesso <cappellessoeleonora@gmail.com>
2024-12-12 16:36:59 +0100

    fixed errors in main component

--- src/components/MainComponent.jsx
+++ src/components/MainComponent.jsx
@@ -1,10 +1,11 @@
 
 function MainComponent({ post }) {
+    console.log(post);
     return (
         <>
             <main className="d-flex justify-content-center align-items-center">
                 <div className="card" style={{ width: "30%" }} key={post.id}>
-                    <img src={post.img} className="card-img-top" alt="..." />
+                    <img src={post.image} className="card-img-top" alt="..." />
                     <div className="card-body">
                         <h5 className="card-title">{post.title}</h5>
                         <p className="card-text">{post.text}</p>
@@ -15,5 +16,7 @@ function MainComponent({ post }) {
         </>
     )
 }
+
+
 
 export default MainComponent;
